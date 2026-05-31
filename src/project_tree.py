c2aa6c68ea6113826ffafe35c8335971c221e0cc
macmarrum <7197131+macmarrum@users.noreply.github.com>
2026-05-31 22:16:56 +0200

    refactor

--- src/project_tree.py
+++ src/project_tree.py
@@ -12,13 +12,13 @@ s = Settings.of(Env.local)
 def print_project_tree():
     pid_to_children = {}
 
-    def print_tree(client: RedmineClient, pid: int, depth=0):
+    def print_tree(client: RedmineClient, parent_id: int | None = None, depth=0):
         indent = '  ' * depth
-        for proj in pid_to_children.get(pid, []):
-            proj: RedmineProject
+        child_projects: list[RedmineProject] = pid_to_children.get(parent_id, [])
+        for proj in child_projects:
             print(f"{indent}{proj.identifier}")
             for isu in client.get_issues(proj.id):
-                print(f"{indent}- {isu.subject}")
+                print(f"{indent}- ({isu.id}) {isu.subject}")
             print_tree(client, proj.id, depth + 1)
 
     with RedmineClient(s.base_url, s.api_key) as c:
@@ -26,7 +26,7 @@ def print_project_tree():
         for project in c.get_projects():
             pid = project.parent.id if project.parent else None
             pid_to_children.setdefault(pid, []).append(project)
-        print_tree(c, None)
+        print_tree(c, parent_id=None)  # start with top-level projects
     pass
 
 
