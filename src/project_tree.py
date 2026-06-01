90bc81b2b6282553a74616c92ed5ae9c1f6f0470
macmarrum <7197131+macmarrum@users.noreply.github.com>
2026-06-01 22:39:48 +0200

    extract get_project_hierarchy

--- src/project_tree.py
+++ src/project_tree.py
@@ -3,10 +3,17 @@
 from __future__ import annotations
 
 from redmine_client import RedmineClient, RedmineProject
-
 from settings import Env, Settings
 
 s = Settings.of(Env.local)
+
+
+def get_project_hierarchy(client: RedmineClient) -> dict[int | None, list[RedmineProject]]:
+    pid_to_children = {}
+    for project in client.get_projects():
+        pid = project.parent.id if project.parent else None
+        pid_to_children.setdefault(pid, []).append(project)
+    return pid_to_children
 
 
 def walk_project_tree(client: RedmineClient, pid_to_children: dict[int | None, list[RedmineProject]], parent_id: int | None = None, level=0):
@@ -17,11 +24,8 @@ def walk_project_tree(client: RedmineClient, pid_to_children: dict[int | None, l
 
 
 def print_project_tree():
-    pid_to_children = {}
     with RedmineClient(s.base_url, s.api_key) as client:
-        for project in client.get_projects():
-            pid = project.parent.id if project.parent else None
-            pid_to_children.setdefault(pid, []).append(project)
+        pid_to_children = get_project_hierarchy(client)
         for proj, level in walk_project_tree(client, pid_to_children, parent_id=None):  # start with top-level
             print(f"{'  ' * level}{{{proj.id}}} {proj.identifier}")
             for isu in client.get_issues(proj.id, subproject_id='!*'):
